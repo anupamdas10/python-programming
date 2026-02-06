e8b324d0797f1992fe0c6a15816b1d97cbf939f4
Anupam <er.anupam10@gmail.com>
2026-02-06 20:37:43 +0530

    stack question

---
+++ practice.py
@@ -0,0 +1,13 @@
+class stack:
+    def func(self,s):
+        stack=[]
+        for i in s:
+            if len(stack) > 0 and stack[-1]==i:
+                stack.pop()
+            else:
+                stack.append(i)
+        return ''.join(stack)
+
+y=stack()
+r=y.func("abbacd")
+print(r)               
