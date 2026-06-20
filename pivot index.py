27a89507e2be7afc49f59cb6e8267d4cb9c65a4d
Anupam <er.anupam10@gmail.com>
2026-06-20 21:42:07 +0530

    pivotindex

---
+++ pivot index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums) :
+        left_sum=0
+        total_sum=sum(nums)
+        for i in range(len(nums)):
+            right_sum=(total_sum-left_sum-nums[i])
+            if left_sum==right_sum:
+                return i
+            left_sum=left_sum+nums[i]
+        return -1
+    
+y=Solution()
+r=y.pivotIndex([1,7,3,6,5]) 
+print(r)   
