5a0e8cd87e4c86087d471c5fdf6ff6cc597ccbd9
Anupam <er.anupam10@gmail.com>
2026-06-30 20:46:20 +0530

    merge interval

---
+++ Merge Intervals.py
@@ -0,0 +1,17 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        merger=[]
+        intervals.sort(key=lambda x: x[0])
+        prev=intervals[0]
+        for i in intervals[1:]:
+            if prev[1]>=i[0]:
+                prev[1] = max(prev[1], i[1])
+            else:
+                merger.append(prev)
+                prev = i
+        merger.append(prev)
+        return merger
+    
+y=Solution()    
+result=y.merge([[1,3],[2,6],[8,10],[15,18]])
+print(result)  
