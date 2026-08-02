c4b175036ca7439204321de96425a01fa7e5fc95
Anupam <er.anupam10@gmail.com>
2026-08-02 11:28:09 +0530

    generate paranthesis

---
+++ generate paranthesis.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        indices=0
+        result=[]
+        total=0
+        brackets=[""]*(2*n)
+        def backtrack(indices,brackets,total,result):
+            if indices>=len(brackets):
+                if total ==0:
+                    result.append("".join(brackets))
+                return result
+            if total>len(brackets)//2:
+                return 
+            if total <0:
+                return
+            brackets[indices]="("
+            backtrack(indices + 1, brackets, total + 1, result)
+            brackets[indices]=")"
+            backtrack(indices + 1, brackets, total - 1, result)
+
+        backtrack(0,brackets,0,result)
+        return result
+
+y=Solution()
+r=y.generateParenthesis(3)
+print(r)
