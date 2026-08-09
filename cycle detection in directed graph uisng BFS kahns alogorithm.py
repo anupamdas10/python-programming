ff1cfc0615839d6cf71a752a86d51f4a375dd321
Anupam <er.anupam10@gmail.com>
2026-08-09 14:12:18 +0530

    cycle detection in directed graph using BFS kahns algorithms

---
+++ cycle detection in directed graph uisng BFS kahns alogorithm.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+
+class Solution:
+    def toposort(self,V,edges):
+        adj_list=[[] for _ in range(V)]
+        indegrees=[0]*V
+        for u, v in edges:
+            adj_list[u].append(v)
+            indegrees[v]+=1
+
+        queue=deque()
+        result=[]   
+
+        for i in range(0,V):
+            if indegrees[i]==0:
+                queue.append(i)
+
+        while len(queue)!=0:
+            current_node=queue.popleft()
+            result.append(current_node)     
+            for adjnode in adj_list[current_node] :
+                indegrees[adjnode]-=1
+                if indegrees[adjnode]==0:
+                    queue.append[adjnode]
+       
+        if len(result)==V:
+            return False
+        else:
+            return True
+
+y=Solution()
+r=y.toposort(4,[[0,1],[0,2],[1,2],[2,0],[2,3]])        
+print(r)
