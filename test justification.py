1dc7106efb25ec638136ca9d77d331c9aecfbaec
Anupam <er.anupam10@gmail.com>
2026-08-23 11:53:17 +0530

    text justification

---
+++ test justification.py
@@ -0,0 +1,51 @@
+from typing import List
+
+class Solution:
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        res = []
+        line, length = [], 0
+        i = 0
+
+        while i < len(words):
+
+            if length + len(line) + len(words[i]) > maxWidth:
+
+                extra_space = maxWidth - length
+
+                spaces = extra_space // max(1, len(line) - 1)
+                remainder = extra_space % max(1, len(line) - 1)
+
+                for j in range(max(1, len(line) - 1)):
+                    line[j] += " " * spaces
+
+                    if remainder > 0:
+                        line[j] += " "
+                        remainder -= 1
+
+                res.append("".join(line))
+
+                line, length = [], 0
+
+            else:
+                line.append(words[i])
+                length += len(words[i])
+                i += 1
+
+        # Last line
+        last_line = " ".join(line)
+        trail_space = maxWidth - len(last_line)
+
+        res.append(last_line + " " * trail_space)
+
+        return res
+
+
+y = Solution()
+
+r = y.fullJustify(["This", "is", "an", "example", "of", "text", "justification."],16)
+
+print(r)
+
+
+
+
