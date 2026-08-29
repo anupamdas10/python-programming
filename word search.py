63d738ea1a8608d9b394deddc3957f318b905f60
Anupam <er.anupam10@gmail.com>
2026-08-29 21:05:54 +0530

    word serach

---
+++ word search.py
@@ -0,0 +1,34 @@
+from typing import List
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows=len(board)
+        cols=len(board[0])
+
+        def dfs(r,c,index):
+            if index==len(word):
+              return True
+
+            if (r<0 or r>=rows or c<0 or c>=cols or board[r][c]!=word[index]):
+                return False
+            temp=board[r][c]
+            board[r][c]="#"    
+
+            found=(dfs(r,c+1,index+1) or dfs(r,c-1,index+1) or dfs(r+1,c,index+1) or dfs(r-1,c,index+1))    
+
+            board[r][c]=temp
+            return found      
+
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c]==word[0]:
+                    if dfs(r,c,0):
+                        return True
+        return False            
+
+y=Solution()
+r=y.exist([["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]],"ABCCED")
+print(r)    
+
+                
+
+      
